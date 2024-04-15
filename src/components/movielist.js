cbf6c61eb61c2d88b755d4465db1e35b2cc27416
lennythepenny <lena.sarieva@gmail.com>
2024-04-14 21:10:28 -0600

    fix movieDetails in movieList

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,3 +1,86 @@
+// import React, { Component } from 'react';
+// import { fetchMovies } from "../actions/movieActions";
+// import { setMovie } from "../actions/movieActions";
+// import { connect } from 'react-redux';
+// import { Image, Nav } from 'react-bootstrap';
+// import { Carousel } from 'react-bootstrap';
+// import { BsStarFill } from 'react-icons/bs'
+// import { LinkContainer } from 'react-router-bootstrap';
+// import MovieDetail from './moviedetail'; 
+
+// class MovieList extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             selectedMovieId: null
+//         };
+//         this.handleSelect = this.handleSelect.bind(this);
+//     }
+
+//     // componentDidMount() {
+//     //     const { dispatch } = this.props;
+//     //     dispatch(fetchMovies());
+//     // }
+//     componentDidMount() {
+//         const { dispatch, movies } = this.props;
+//         if (movies.length > 0) {
+//             const firstMovieId = movies[0]._id;
+//             dispatch(setMovie(movies[0]));
+//             this.setState({ selectedMovieId: firstMovieId });
+//         }
+//         dispatch(fetchMovies());
+//     }
+
+
+//     handleSelect(selectedIndex, e) {
+//         const { dispatch, movies } = this.props;
+//         const selectedMovie = movies[selectedIndex];
+//         dispatch(setMovie(selectedMovie));
+//         this.setState({ selectedMovieId: selectedMovie._id });
+//     }
+
+//     handleClick = (movie) => {
+//         const { dispatch } = this.props;
+//         dispatch(setMovie(movie));
+//         this.setState({ selectedMovieId: movie._id });
+//     }
+
+//     render() {
+//         const { movies } = this.props;
+//         const { selectedMovieId } = this.state;
+
+//         return (
+//             <div>
+//                 <Carousel onSelect={this.handleSelect}>
+//                     {movies && movies.map((movie) =>
+//                         <Carousel.Item key={movie._id}>
+//                             <div>
+//                                 <LinkContainer to={'/movie/'+movie._id} onClick={() => this.handleClick(movie)}>
+//                                     <Nav.Link><Image className="image" src={movie.imageUrl} thumbnail /></Nav.Link>
+//                                 </LinkContainer>
+//                             </div>
+//                             <Carousel.Caption>
+//                                 <h3>{movie.title}</h3>
+//                                 <BsStarFill glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+//                             </Carousel.Caption>
+//                         </Carousel.Item>
+//                     )}
+//                 </Carousel>
+//                 {/* Render MovieDetail component only for the selected movie */}
+//                 {selectedMovieId && <MovieDetail movieId={selectedMovieId} />}
+//             </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return {
+//         movies: state.movie.movies
+//     }
+// }
+
+// export default connect(mapStateToProps)(MovieList);
+
 import React, { Component } from 'react';
 import { fetchMovies } from "../actions/movieActions";
 import { setMovie } from "../actions/movieActions";
@@ -17,20 +100,10 @@ class MovieList extends Component {
         this.handleSelect = this.handleSelect.bind(this);
     }
 
-    // componentDidMount() {
-    //     const { dispatch } = this.props;
-    //     dispatch(fetchMovies());
-    // }
     componentDidMount() {
-        const { dispatch, movies } = this.props;
-        if (movies.length > 0) {
-            const firstMovieId = movies[0]._id;
-            dispatch(setMovie(movies[0]));
-            this.setState({ selectedMovieId: firstMovieId });
-        }
+        const { dispatch } = this.props;
         dispatch(fetchMovies());
     }
-
 
     handleSelect(selectedIndex, e) {
         const { dispatch, movies } = this.props;
@@ -80,4 +153,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(MovieList);
-
