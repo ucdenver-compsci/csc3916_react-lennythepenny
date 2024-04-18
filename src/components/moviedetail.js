8c5e255b3d774521b3990ff4d9125c83e27d0ee5
lennythepenny <lena.sarieva@gmail.com>
2024-04-18 10:17:04 -0600

    trying to get reviewForm to show user when posting review

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,3 +1,78 @@
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { fetchMovie } from "../actions/movieActions";
+// import { Card, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap';
+// import { BsStarFill } from 'react-icons/bs';
+// import { Image } from 'react-bootstrap';
+// import ReviewForm from './reviewform';
+
+// class MovieDetail extends Component {
+//     componentDidMount() {
+//         const { dispatch, selectedMovie, movieId} = this.props;
+//         if (!selectedMovie) {
+//             dispatch(fetchMovie(movieId));
+//         }
+//     }
+
+//     render() {
+//         const { selectedMovie, movieId} = this.props;
+
+//         if (!selectedMovie) {
+//             return <div>Loading....</div>;
+//         }
+
+//         return (
+//             <Card>
+//                 <Card.Header>Movie Detail</Card.Header>
+//                 <Card.Body>
+//                     {selectedMovie.imageUrl && (
+//                         <Image className="image" src={selectedMovie.imageUrl} thumbnail />
+//                     )}
+//                     <ListGroup>
+//                         <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+//                         <ListGroupItem>
+//                             {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
+//                                 <p key={i}>
+//                                     <b>{actor.actorName}</b> {actor.characterName}
+//                                 </p>
+//                             ))}
+//                         </ListGroupItem>
+//                         <ListGroupItem>
+//                             <h4><BsStarFill/> {selectedMovie.avgRating}</h4>
+//                         </ListGroupItem>
+//                         <ListGroupItem>
+//                             <h5>Reviews:</h5>
+//                             <Row>
+//                                 {selectedMovie.movie_reviews && selectedMovie.movie_reviews.map((review, i) => (
+//                                     <Col key={i} xs={12} sm={6} md={4} lg={3}>
+//                                         <div style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
+//                                             <p style={{ fontWeight: 'bold' }}>{review.username}</p>
+//                                             <p>{review.review}</p>
+//                                             <p><BsStarFill /> {review.rating}</p>
+//                                         </div>
+//                                     </Col>
+//                                 ))}
+//                             </Row>
+//                         </ListGroupItem>
+//                     </ListGroup>
+//                 </Card.Body>
+//                 <Card.Body>
+//                     <h5>Leave a Review</h5>
+//                     <ReviewForm movieId={movieId}/>
+//                 </Card.Body>
+//             </Card>
+//         );
+//     }
+// }
+
+// const mapStateToProps = (state, ownProps) => {
+//     return {
+//         selectedMovie: state.movie.selectedMovie,
+//         movieId: ownProps.movieId,
+//     }
+// }
+
+// export default connect(mapStateToProps)(MovieDetail);
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchMovie } from "../actions/movieActions";
@@ -15,7 +90,7 @@ class MovieDetail extends Component {
     }
 
     render() {
-        const { selectedMovie, movieId} = this.props;
+        const { selectedMovie, movieId, username } = this.props; // Add username to props
 
         if (!selectedMovie) {
             return <div>Loading....</div>;
@@ -58,7 +133,8 @@ class MovieDetail extends Component {
                 </Card.Body>
                 <Card.Body>
                     <h5>Leave a Review</h5>
-                    <ReviewForm movieId={movieId}/>
+                    {/* Pass username to ReviewForm */}
+                    <ReviewForm movieId={movieId} username={username} />
                 </Card.Body>
             </Card>
         );
@@ -69,6 +145,7 @@ const mapStateToProps = (state, ownProps) => {
     return {
         selectedMovie: state.movie.selectedMovie,
         movieId: ownProps.movieId,
+        username: state.auth.username // Assuming username is stored in auth state
     }
 }
 
--- src/components/reviewform.js
+++ src/components/reviewform.js
@@ -1,140 +1,140 @@
-// import React, { useState } from 'react';
-// import { Form, Button, Alert } from 'react-bootstrap';
-// import { useDispatch } from 'react-redux';
-// import { addReview } from '../actions/reviewActions';
-// import { useSelector } from 'react-redux';
+// import React, { Component } from 'react';
+// import { connect, useSelector } from 'react-redux'; // Importing useSelector
+// import { fetchMovie } from "../actions/movieActions";
+// import { Card, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap';
+// import { BsStarFill } from 'react-icons/bs';
+// import { Image } from 'react-bootstrap';
+// import ReviewForm from './reviewform';
 
-// const ReviewForm = ({ movieId }) => {
-//   const dispatch = useDispatch();
-//   const [formData, setFormData] = useState({
-//     rating: '',
-//     review: ''
-//   });
-//   const [error, setError] = useState('');
-
-//   const {rating, review } = formData;
-
-//   const onChange = e => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
-
-//   const onSubmit = e => {
-//     e.preventDefault();
-//     if (!rating || !review) {
-//       setError('Please provide a rating and a review.');
-//     } else {
-//       dispatch(addReview(movieId, {username, rating, review}));
-//       setFormData({rating: '', review: '' });
-//       setError('');
+// class MovieDetail extends Component {
+//     componentDidMount() {
+//         const { dispatch, selectedMovie, movieId} = this.props;
+//         if (!selectedMovie) {
+//             dispatch(fetchMovie(movieId));
+//         }
 //     }
-//   };
 
-//   return (
-//     <Form onSubmit={onSubmit}>
-//       {error && <Alert variant="danger">{error}</Alert>}
-//       <Form.Group controlId="rating">
-//         <Form.Label>Rating</Form.Label>
-//         <Form.Control
-//           type="number"
-//           name="rating"
-//           value={rating}
-//           onChange={onChange}
-//           min="1"
-//           max="5"
-//           required
-//         />
-//       </Form.Group>
-//       <Form.Group controlId="review">
-//         <Form.Label>Review</Form.Label>
-//         <Form.Control
-//           as="textarea"
-//           rows={3}
-//           name="review" 
-//           value={review}
-//           onChange={onChange}
-//           required
-//         />
-//       </Form.Group>
-//       <Button variant="primary" type="submit">
-//         Submit
-//       </Button>
-//     </Form>
-//   );
-// };
+//     render() {
+//         const { selectedMovie, movieId } = this.props;
 
-// export default ReviewForm;
-import React, { Component } from 'react';
-import { connect, useSelector } from 'react-redux'; // Importing useSelector
-import { fetchMovie } from "../actions/movieActions";
-import { Card, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap';
-import { BsStarFill } from 'react-icons/bs';
-import { Image } from 'react-bootstrap';
-import ReviewForm from './reviewform';
+//         if (!selectedMovie) {
+//             return <div>Loading....</div>;
+//         }
 
-class MovieDetail extends Component {
-    componentDidMount() {
-        const { dispatch, selectedMovie, movieId} = this.props;
-        if (!selectedMovie) {
-            dispatch(fetchMovie(movieId));
+//         return (
+//             <Card>
+//                 <Card.Header>Movie Detail</Card.Header>
+//                 <Card.Body>
+//                     {selectedMovie.imageUrl && (
+//                         <Image className="image" src={selectedMovie.imageUrl} thumbnail />
+//                     )}
+//                     <ListGroup>
+//                         <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+//                         <ListGroupItem>
+//                             {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
+//                                 <p key={i}>
+//                                     <b>{actor.actorName}</b> {actor.characterName}
+//                                 </p>
+//                             ))}
+//                         </ListGroupItem>
+//                         <ListGroupItem>
+//                             <h4><BsStarFill/> {selectedMovie.avgRating}</h4>
+//                         </ListGroupItem>
+//                         <ListGroupItem>
+//                             <h5>Reviews:</h5>
+//                             <Row>
+//                                 {selectedMovie.movie_reviews && selectedMovie.movie_reviews.map((review, i) => (
+//                                     <Col key={i} xs={12} sm={6} md={4} lg={3}>
+//                                         <div style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
+//                                             <p style={{ fontWeight: 'bold' }}>{review.username}</p>
+//                                             <p>{review.review}</p>
+//                                             <p><BsStarFill /> {review.rating}</p>
+//                                         </div>
+//                                     </Col>
+//                                 ))}
+//                             </Row>
+//                         </ListGroupItem>
+//                     </ListGroup>
+//                 </Card.Body>
+//                 <Card.Body>
+//                     <h5>Leave a Review</h5>
+//                     <ReviewForm movieId={movieId} /> 
+//                 </Card.Body>
+//             </Card>
+//         );
+//     }
+// }
+
+// const mapStateToProps = (state, ownProps) => {
+//     return {
+//         selectedMovie: state.movie.selectedMovie,
+//         movieId: ownProps.movieId,
+//     }
+// }
+
+// export default connect(mapStateToProps)(MovieDetail);
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { addReview } from '../actions/reviewActions';
+
+const ReviewForm = ({ movieId, username }) => { // Receive username prop
+    const dispatch = useDispatch();
+    const [formData, setFormData] = useState({
+        rating: '',
+        review: ''
+    });
+    const [error, setError] = useState('');
+
+    const { rating, review } = formData;
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if (!rating || !review) {
+            setError('Please provide a rating and a review.');
+        } else {
+            // Include username when dispatching addReview action
+            dispatch(addReview(movieId, { username, rating, review }));
+            setFormData({ rating: '', review: '' });
+            setError('');
         }
-    }
+    };
 
-    render() {
-        const { selectedMovie, movieId } = this.props;
+    return (
+        <Form onSubmit={onSubmit}>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form.Group controlId="rating">
+                <Form.Label>Rating</Form.Label>
+                <Form.Control
+                    type="number"
+                    name="rating"
+                    value={rating}
+                    onChange={onChange}
+                    min="1"
+                    max="5"
+                    required
+                />
+            </Form.Group>
+            <Form.Group controlId="review">
+                <Form.Label>Review</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={3}
+                    name="review"
+                    value={review}
+                    onChange={onChange}
+                    required
+                />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+};
 
-        if (!selectedMovie) {
-            return <div>Loading....</div>;
-        }
-
-        return (
-            <Card>
-                <Card.Header>Movie Detail</Card.Header>
-                <Card.Body>
-                    {selectedMovie.imageUrl && (
-                        <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-                    )}
-                    <ListGroup>
-                        <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-                        <ListGroupItem>
-                            {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
-                                <p key={i}>
-                                    <b>{actor.actorName}</b> {actor.characterName}
-                                </p>
-                            ))}
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <h4><BsStarFill/> {selectedMovie.avgRating}</h4>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <h5>Reviews:</h5>
-                            <Row>
-                                {selectedMovie.movie_reviews && selectedMovie.movie_reviews.map((review, i) => (
-                                    <Col key={i} xs={12} sm={6} md={4} lg={3}>
-                                        <div style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
-                                            <p style={{ fontWeight: 'bold' }}>{review.username}</p>
-                                            <p>{review.review}</p>
-                                            <p><BsStarFill /> {review.rating}</p>
-                                        </div>
-                                    </Col>
-                                ))}
-                            </Row>
-                        </ListGroupItem>
-                    </ListGroup>
-                </Card.Body>
-                <Card.Body>
-                    <h5>Leave a Review</h5>
-                    <ReviewForm movieId={movieId} /> 
-                </Card.Body>
-            </Card>
-        );
-    }
-}
-
-const mapStateToProps = (state, ownProps) => {
-    return {
-        selectedMovie: state.movie.selectedMovie,
-        movieId: ownProps.movieId,
-    }
-}
-
-export default connect(mapStateToProps)(MovieDetail);
+export default ReviewForm;
